798e9f3b10808a6eb26863871e2ee6822fc77db7
Arnol <72963047@certus.edu.pe>
2024-09-29 16:27:18 -0500

    s

--- registro.js
+++ registro.js
@@ -1,9 +1,4 @@
-const express = require('express');
-const router = express.Router();
-const User = require('./models/User'); // Ajusta la ruta según sea necesario
-const registro = require('./registr')
-// Ruta para registrar un nuevo usuario
-router.post('/registro', async (req, res) => {
+router.post('/register', async (req, res) => {
     const { nombre, correo, password } = req.body;
 
     try {
@@ -15,12 +10,3 @@ router.post('/registro', async (req, res) => {
         res.status(500).json({ message: 'Error en el registro', error });
     }
 });
-const response = await fetch('http://localhost:3000/api/register', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(userData)
-});
-
-module.exports = router;
