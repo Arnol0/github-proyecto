c544eca0733a639810e04944b413cfabf069ee6b
Arnol <72963047@certus.edu.pe>
2024-09-29 19:49:53 -0500

    h

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
-// models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
-    correo: { type: String, required: true, unique: true }, // Asegúrate de que el correo sea único
-    contraseña: { type: String, required: true } // Esta propiedad debe coincidir con el nombre en el registro
+    correo: { type: String, required: true, unique: true },
+    contraseña: { type: String, required: true }
 });
+
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
