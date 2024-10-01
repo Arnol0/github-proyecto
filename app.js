7ce2a13bba90e15fa5f518d53adc34b2c8b0878a
Arnol <72963047@certus.edu.pe>
2024-10-01 08:04:09 -0500

    f

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Servir archivos estáticos desde la carpeta 'paginas'
 app.use(express.static(path.join(__dirname, 'paginas')));
 
+const mongo_uri = process.env.MONGO_URI;
 // Conexión a MongoDB
-const mongo_uri = 'mongodb+srv://72963047:cAZKWOSTxC57BxJK@cluster0.wjj7u.mongodb.net/Información?retryWrites=true&w=majority&appName=Cluster0';
+//const mongo_uri = 'mongodb+srv://72963047:cAZKWOSTxC57BxJK@cluster0.wjj7u.mongodb.net/Información?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log(`Conectado correctamente a MongoDB`))
