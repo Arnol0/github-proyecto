d95265bb9afefca0c4c74b0a1cebf9b42dd7308d
Arnol <72963047@certus.edu.pe>
2024-10-15 18:30:04 -0500

    s

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const User = require('./models/user'); // Asegúrate de que la ruta es correcta
+const User = require('../models/User'); // Asegúrate de que la ruta es correcta
 const router = express.Router();
 
 router.post('/login', async (req, res) => {
